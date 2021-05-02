7ddfd87b4f32f9be3d90f83ce3a8303c61c3a58c
ed ben <edwardbenedict92@gmail.com>
2021-05-02 15:12:49 +0300

    feat: cahnge image

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <Card
         title="Lorem Ipsum"
-        imageUrl="https://images.pexels.com/photos/1714208/pexels-photo-1714208.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+        imageUrl="https://www.mediaclick.com.tr/uploads/2020/05/lorem-ipsum.png"
         body="Lorem Ipsum is simply dummy text of the printing and 
               typesetting industry. Lorem Ipsum has been the industry's 
               standard dummy text ever since the 1500s."
