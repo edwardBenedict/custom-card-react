a37097b6e296011e874d7096dae90b221a205c02
ed ben <edwardbenedict92@gmail.com>
2021-05-02 15:10:45 +0300

    feat: change styling and content

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ function App() {
       <Card
         title="Card Title"
         imageUrl="https://images.pexels.com/photos/1714208/pexels-photo-1714208.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-        body="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        body="Lorem Ipsum is simply dummy text of the printing and 
+              typesetting industry. Lorem Ipsum has been the industry's 
+              standard dummy text ever since the 1500s."
       />
     </div>
   );
